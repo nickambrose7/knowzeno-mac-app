08683adca9cf26fbe6092971b479aa78df8eac5b
Nick Ambrose <nickambrose7@gmail.com>
2026-04-26 19:06:46 -0700

    Initial Commit

---
+++ knowzeno/knowzenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  knowzenoApp.swift
+//  knowzeno
+//
+//  Created by Nick Ambrose on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct knowzenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ knowzenoTests/knowzenoTests.swift
@@ -0,0 +1,19 @@
+//
+//  knowzenoTests.swift
+//  knowzenoTests
+//
+//  Created by Nick Ambrose on 4/26/26.
+//
+
+import Testing
+@testable import knowzeno
+
+struct knowzenoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
